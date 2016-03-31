24037783150ae098a334e755e574a697eb6eeb53
Louisa Reese <louisa.e.reese@gmail.com>
2016-03-31 13:22:32 -0600

    refactored mad libs

--- Unit-1/app.js
+++ Unit-1/app.js
@@ -2,9 +2,19 @@ var app = angular.module("firstApp", []);
 app.controller("MyFirstController", function ($scope) {
   $scope.name = "Severus Snape";
 });
-
+// Controllers manage the view
 app.controller("ExercisesController", function ($scope) {
   $scope.FavColor = 'favoritecolor';
   $scope.secondsInACentury = 60*60*24*365*100;
   $scope.rightNow = new Date();
+});
+
+app.controller('MadLibs', function ($scope) {
+  $scope.boyName = "";
+  $scope.adjective = "";
+  $scope.pluralNoun = "";
+  $scope.pluralNoun2 = "";
+  $scope.insect = "";
+  $scope.pluralNoun3 = "";
+  $scope.verb = "";
 });
